f2fc8122dd43de257f11de34dd38bab2107daa05
ProFunDS <68074080+ProFunDS@users.noreply.github.com>
2020-07-13 01:30:24 +0300

    Update philipp.py

--- philipp.py
+++ philipp.py
@@ -23,6 +23,23 @@ champions = ['Азира', 'Акали', 'Алистара', 'Амуму', 'Ан
 	"Треша", "Триндамира", "Тристану", "Удира", "Ургота", "Фиддлстикса", "Физза", "Фиору :person_fencing:", "Хеймердингера", 
 	"ЧоГата", "Шако", "Шаю", "Шена", "Шивану", "Эвелинн", "Эзреаля", "Экко", "Элизу", "Энни :fire:", "Эш", "Юми :smirk_cat:", 
 	"Ясуо :man_in_manual_wheelchair_tone1:", "Лиллию", 'Олафа', "Виктора"]
+top = ["Акали", "Атрокса", "Вейн", "Владимира", "Волибира", "Вуконга", "Гангпланка", "Гарена", "Гнара", "Дариуса", "Джакса", 
+	"Джейса", "Доктора Мундо", "Иллаой", "Ирелию", "Йорика", "Камиллу", "Квинн", "Кейл", "Кеннена", "Кледа", "Мальфита", "Маокая",
+	"Мордекайзера", "Насуса", "Орна", "Поппи", "Рамбла", "Ренектона", "Ривен", "Сайласа", "Сетта", "Синджеда", "Сиона", "Тимо",
+	"Триндамира", "Ургота", "Фиору", "Чо'Гата", "Шена", "Ясуо"]
+jungle = ["Амуму", "Вай", "Варвика", "Волибира", "Вуконга", "Гекарима", "Грагаса", "Грейвза", "Джакса", "Джарвана", "Зака", "Иверна",
+	"Ка'Зикса", "Каина", "Картуса", "Киндред", "Ксин Жао", "Ли Сина", "Мастера Йи", "Нидали", "Ноктюрна", "Нуну и Виллумпа", "Олафа",
+	"Раммуса", "Рек'Сай", "Ренгара", "Сайласа", "Седжуани", "Сетта", "Скарнера", "Талию", "Трандла", "Удира", "Фиддлстика", "Шако",
+	"Шивану", "Эвелинн", "Экко", "Элизу"]
+mid = ["Азира", "Акали", "Анивию", "Ари", "Аурелион Сола", "Вейгара", "Вел'Коза", "Виктора", "Владимира", "Галио", "Диану", "Зеда",
+	"Зерата", "Зиггса", "Зои", "Ирелию", "Кассадина", "Кассиопею", "Катарину", "Киану", "Корки", "Ле Блан", "Лиссандру", "Люкс",
+	"Мальзахара", "Нико", "Орианну", "Пантеона", "Райза", "Рамбла", "Сайласа", "Синдру", "Талона", "Твистед Фэйта", "Физза",
+	"Хеймердингера", "Экко", "Энни", "Ясуо"]
+bot = ["Афелия"," Варуса", "Вейна", "Джина", "Джинкс", "Дрейвена", "Кай'Су", "Калисту", "Кейтлин", "Ког'Мао", "Люциан", "Мисс Фортуну",
+	"Сивир", "Твича", "Тристану", "Шаю", "Эзреаля", "Эш", "Ясуо"]
+support = ["Алистара", "Барда", "Блицранка", "Браума", "Брэнда", "Вел'Коза", "Жанну", "Зайру", "Зерата", "Зилеана", "Карму", "Леону",
+	"Лулу", "Люкс", "Мальфита", "Моргану", "Нами", "Наутилуса", "Пайка", "Рэйкана", "Свейна", "Сенну", "Сетта", "Сона", "Сораку",
+	"Таам Кенча", "Тарика", "Треша", "Юми"]
 
 motivations = ['. Я думаю, это будет легко', '. Я думаю, это будет просто', '. Я думаю, это не сложно', 
 	'. Я думаю, ты справишься', '. Я верю в тебя', '. Ты справишься', '. Я думаю, ты сможешь', 
@@ -44,15 +61,52 @@ async def on_member_join(member):
 
 @Phil.command(aliases = ['admins'])
 async def admin(ctx):
-	await ctx.send(f'**#4708 Эльдар** :fleur_de_lis: \n**#5665 Филя** <:morgana:722944166262472734> \n**#0641 Тёма <:kayn:723183895100653638>** \n**#1952 Даня** <:jinx:722932714977886298> \n**#6994 Аня** <:anya:722625155016163378>')
+	await ctx.send(f'**#4708 Эльдар** :fleur_de_lis: \n**#5665 Филя** <:morgana:722944166262472734> \n**#0641 Тёма <:kayn:723183895100653638> ** \n**#1952 Даня** <:jinx:722932714977886298> \n**#6994 Аня** <:anya:722625155016163378>  ')
+
 
 @Phil.command(pass_context=True)
-async def change(ctx):
-	champion = random.choice(champions)
-	motivation = random.choice(motivations)
-	emoji = random.choice(emojis)
-	emoj = random.choice(emojis)
-	await ctx.send(f"{emoj} Попробуй **{champion}**{motivation} {emoji}")
+async def change(ctx, lane: str = None):
+	author = ctx.message.author
+	if lane is None:
+		champion = random.choice(champions)
+		motivation = random.choice(motivations)
+		emoji = random.choice(emojis)
+		emoj = random.choice(emojis)
+		await ctx.message.delete()
+		await ctx.send(f"{emoj} {author.mention}, попробуй **{champion}**{motivation} {emoji}")
+	elif lane == 'top':
+		tp = random.choice(top)
+		emoji = random.choice(emojis)
+		emoj = random.choice(emojis)
+		await ctx.message.delete()
+		await ctx.send(f'{emoji} {author.mention}, возьми **{tp}** {emoj}')
+	elif lane == 'jungle':
+		jg = random.choice(jungle)
+		emoji = random.choice(emojis)
+		emoj = random.choice(emojis)
+		await ctx.message.delete()
+		await ctx.send(f'{emoji} {author.mention}, возьми **{jg}** {emoj}')
+	elif lane == 'mid':
+		md = random.choice(mid)
+		emoji = random.choice(emojis)
+		emoj = random.choice(emojis)
+		await ctx.message.delete()
+		await ctx.send(f'{emoji} {author.mention}, возьми **{md}** {emoj}')
+	elif lane == 'bot':
+		bt = random.choice(bot)
+		emoji = random.choice(emojis)
+		emoj = random.choice(emojis)
+		await ctx.message.delete()
+		await ctx.send(f'{emoji} {author.mention}, возьми **{bt}** {emoj}')
+	elif lane == 'sup' or lane == 'support':
+		sp = random.choice(support)
+		emoji = random.choice(emojis)
+		emoj = random.choice(emojis)
+		await ctx.message.delete()
+		await ctx.send(f'{emoji} {author.mention}, возьми **{sp}** {emoj}')
+	else:
+		await ctx.message.delete()
+		await ctx.send(f'**{author.mention}, неверно указана линия.** \n **```(top | jungle | mid | bot | support)```**')
 
 @Phil.command(pass_context=True) 
 async def help(ctx):
